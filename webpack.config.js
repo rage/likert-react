8fa2387eb5948c7806c9c1e28b30ad562e578eea
Henrik Nygren <nygrenh@gmail.com>
2017-08-14 17:11:26 +0300

    Version 0.0.0-beta1

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-/*
- * We've enabled UglifyJSPlugin for you! This minifies your app
- * in order to load faster and run less javascript.
- *
- * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
- *
- */
-
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
-/*
- * We've enabled Postcss, autoprefixer and precss for you. This allows your app
- * to lint  CSS, support variables and mixins, transpile future CSS syntax,
- * inline images, and more!
- *
- * To enable SASS or LESS, add the respective loaders to module.rules
- *
- * https://github.com/postcss/postcss
- *
- * https://github.com/postcss/autoprefixer
- *
- * https://github.com/jonathantneal/precss
- *
- */
-
 const autoprefixer = require('autoprefixer');
 const precss = require('precss');
 
